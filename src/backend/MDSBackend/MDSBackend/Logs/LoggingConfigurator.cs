df28422f14e870ab03fc747b5492b3566c98dd1f
ereshkigal <roman.khlynin@bk.ru>
2025-03-15 11:54:42 +0300

    fix: Added LoggingConfigurator.cs

---
+++ src/backend/MDSBackend/MDSBackend/Logs/LoggingConfigurator.cs
@@ -0,0 +1,24 @@
+using System.Reflection;
+using Serilog;
+using Serilog.Exceptions;
+
+namespace MDSBackend.Logs;
+
+public static class LoggingConfigurator
+{
+    public static void ConfigureLogging(){
+        var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production";
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json",optional:false,reloadOnChange:true).Build();
+        Console.WriteLine(environment);
+        Console.WriteLine(configuration);
+        Log.Logger = new LoggerConfiguration()
+            .Enrich.FromLogContext()
+            .Enrich.WithExceptionDetails()
+            .WriteTo.Debug()
+            .WriteTo.Console()
+            .Enrich.WithProperty("Environment",environment)
+            .ReadFrom.Configuration(configuration)
+            .CreateLogger();
+    }
+}
